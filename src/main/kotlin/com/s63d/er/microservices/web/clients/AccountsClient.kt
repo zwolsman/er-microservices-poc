4eb8a34ea38e074d6b07895e841fb9f4a0e7fa02
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-18 15:46:47 +0100

    Added bootstrap & styling Now including a header in the 2 tempaltes

--- src/main/kotlin/com/s63d/er/microservices/web/clients/AccountsClient.kt
+++ src/main/kotlin/com/s63d/er/microservices/web/clients/AccountsClient.kt
@@ -2,13 +2,12 @@ package com.s63d.er.microservices.web.clients
 
 import com.s63d.er.microservices.accounts.models.Account
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
 @Component
-class AccountsClient {
+class AccountsClient(private val restTemplate: RestTemplate) {
     companion object {
         val logger = LoggerFactory.getLogger(this::class.java)!!
     }
@@ -16,10 +15,8 @@ class AccountsClient {
     @Value("\${services.accounts}")
     lateinit var BASE_URL: String
 
-    @Autowired
-    private lateinit var restTemplate: RestTemplate
-
     private val logger = LoggerFactory.getLogger(this::class.java)!!
+
     fun all(): Array<Account> {
         logger.info("Getting all the accounts")
         return restTemplate.getForObject(BASE_URL, Array<Account>::class.java)!!
