0f294bd49dcc75fde4144701d401d1fc638607f5
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-18 13:00:02 +0100

    initial commit

---
+++ src/main/kotlin/com/s63d/er/microservices/ErMicroservicesApplication.kt
@@ -0,0 +1,11 @@
+package com.s63d.er.microservices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ErMicroservicesApplication
+
+fun main(args: Array<String>) {
+    runApplication<ErMicroservicesApplication>(*args)
+}
---
+++ src/test/kotlin/com/s63d/er/microservices/ErMicroservicesApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.s63d.er.microservices
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ErMicroservicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
