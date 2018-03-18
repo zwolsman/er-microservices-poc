490564e08a5afdf38057e428c37a31fb5a67fc4e
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-18 14:01:24 +0100

    more renaming

--- src/main/kotlin/com/s63d/er/microservices/web/controllers/TestController.kt
+++ src/main/kotlin/com/s63d/er/microservices/web/controllers/TestController.kt
@@ -1,14 +1,14 @@
 package com.s63d.er.microservices.web.controllers
 
-import com.s63d.er.microservices.web.clients.AccountClient
+import com.s63d.er.microservices.web.clients.AccountsClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TestController(private val accountClient: AccountClient) {
+class TestController(private val accountsClient: AccountsClient) {
 
     @GetMapping
     fun index() {
-        accountClient.test()
+        accountsClient.test()
     }
 }
